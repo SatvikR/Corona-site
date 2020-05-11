072f68cc87494a87aba6e3ce11d9659f56566d5f
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-05-11 16:59:34 -0700

    Fixed bugs with multiple users at the same time
    There was a problem where when two people were on at the same time, one users' data would be overwritten by another. I added SQL-alchemy which stores users temporarily by IP address to stop this.

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ import json
 import corona
 import csv
 import requests
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "sfdhsdklfjwerjfcksldfjkdlsfjekl"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICTIONS"] = False
+
 raw_data = (
     "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 )
 selected = 'False'
+# test
 
+db = SQLAlchemy(app)
 def dump_data(state, county, file, x, y, json_file):
     corona.get_data(x, y, file, state, county)
     data = {
@@ -22,6 +28,17 @@ def dump_data(state, county, file, x, y, json_file):
     with open(json_file, 'w') as outfile:
         json.dump(data, outfile, indent=4)
 
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    ip = db.Column("ip", db.String(100))
+    state = db.Column("state", db.String(100))
+    county = db.Column("county", db.String(100))
+
+
+    def __init__(self, ip, state, county):
+        self.ip = ip
+        self.state = state
+        self.county = county
 
 
 @app.route('/', methods=["POST", "GET"])
@@ -41,18 +58,25 @@ def home():
         state = request.form['state']
         county = request.form['county']
         if state == 'default' or county == 'county':
-            data = json.load(open('data.json'))
-            jtopy = json.dumps(data) 
-            dict_json = json.loads(jtopy) 
-            state = dict_json['state']
-            county = dict_json['county']
-        print(state, county)
+            state = 'California'
+            county = 'San Francisco'
         file = corona.get_file(raw_data)
         x = []
         y = []
         dump_data(state, county, file, x, y, 'data.json')
 
-        selected = 'True'
+        corona.get_data(x, y, file, state, county)
+        ip_addr = request.remote_addr
+        found_user = users.query.filter_by(ip=ip_addr).first()
+        if found_user:
+            found_user.state = state
+            found_user.county = county
+            db.session.commit()
+        else:
+            user = users(ip_addr, state, county)
+            db.session.add(user)
+            db.session.commit()
+            
         file = corona.get_file(raw_data)
         states = corona.get_states(file)
         keys = []
@@ -61,6 +85,7 @@ def home():
         print(selected)
         return render_template("index.html", states=states, len=len(states), len2=len(keys), keys=keys, values=values, selected='True')
     else:
+        ip_addr = request.remote_addr
         file = corona.get_file(raw_data)
         states = corona.get_states(file)
         keys = []
@@ -74,12 +99,22 @@ def home():
         dict_json = json.loads(jtopy) 
         state = dict_json['state']
         county = dict_json['county']
-
+        found_user = users.query.filter_by(ip=ip_addr).first()
+        if found_user:
+            found_user.state = state
+            found_user.county = county
+            db.session.commit()
+        else:
+            user = users(ip_addr, state, county)
+            db.session.add(user)
+            db.session.commit()
         dump_data(state, county, file, x, y, 'data.json')
+        file = corona.get_file(raw_data)
         return render_template("index.html", states=states, len=len(states), len2=len(keys), keys=keys, values=values, selected=selected) 
 
 @app.route('/send', methods=["POST", "GET"])
 def send():
+    ip_addr = request.remote_addr
     if request.method == "POST":
         return redirect(url_for("home"))
     data = json.load(open('data.json'))
@@ -88,7 +123,14 @@ def send():
     print(jsonify(dict_json))
     x = dict_json['x']
     y = dict_json['y']
-    return render_template("graph.html", state=dict_json['state'], county=dict_json['county'], json_data=dict_json, x=x, y=y)
+    found_user = users.query.filter_by(ip=ip_addr).first()
+    state = found_user.state
+    county = found_user.county
+    file = corona.get_file(raw_data)
+    x = []
+    y = []
+    corona.get_data(x,y, file, state, county)
+    return render_template("graph.html", state=state, county=county, json_data=dict_json, x=x, y=y)
 
 @app.route('/counties', methods=['POST', 'GET'])
 def counties():
@@ -127,4 +169,5 @@ def graph():
     return redirect(url_for("send"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    db.create_all()
+    app.run(host='0.0.0.0')
